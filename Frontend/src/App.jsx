5d7637e8fb3a4b3b29ba7bd959c3d1dc6977bc5f
Deepak <dpkg2831@gmail.com>
2025-09-15 08:59:28 +0530

    UPDATE: Login Page Created

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,13 +1,15 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Chat from "./pages/Chat";
-import Login from './pages/Login'
+import Login from './pages/Login';
 import { AuthProvider } from "./context/AuthContext";
 import Register from "./pages/Register";
+import Navbar2 from "./components/Navbar/Navbar2"; // <-- Import Navbar2
 
 function App() {
   return (
     <BrowserRouter>
+      <Navbar2 /> {/* <-- Add this line */}
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/chat" element={<Chat />} />
